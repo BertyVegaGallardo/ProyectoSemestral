c438466328adec6e87fe3fd47389dec99086a3ce
Pip3x <fafb2001@gmail.com>
2021-05-05 19:10:25 -0400

    Modificacion de index, registrarse e iniciar sesion

--- proyectoSemestral/js/Registrarse.js
+++ proyectoSemestral/js/Registrarse.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
     $('#show').mousedown(function(){
         $('#txtContrasenia').removeAttr('type');
+        $('#show').addClass('fa-eye-slash').removeClass('fa-eye');
     });
 
     $('#show').mouseup(function(){
-        $('#txtContrasenia').atttr('type','password');
+        $('#txtContrasenia').attr('type','password');
+        $('#show').addClass('fa-eye').removeClass('fa-eye-slash');
     });
 });
